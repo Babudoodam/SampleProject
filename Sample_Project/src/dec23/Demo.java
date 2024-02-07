f3c5bc23fd41bde1645b321fafe67ac3a0512cf0
babu <babudoodam218@gmail.com>
2024-02-07 15:58:22 +0530

    Demo in SampleProject

---
+++ Sample_Project/src/dec23/Demo.java
@@ -0,0 +1,12 @@
+package dec23;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		String a = "ABCD";
+		String b = "XYZ";
+		String c = "   ";
+		System.out.println(a+c.concat(b));
+	}
+
+}
